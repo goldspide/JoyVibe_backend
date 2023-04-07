5ab1983425c1b96d39b10ee3df127e7f2389a7dd
goldspide <raoulbonsso@gmail.com>
2023-04-07 12:57:51 +0100

    mise sur pied des different class

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,10 @@
-export const schemaTypes = []
+import user from './user';
+import pin from './pin';
+import postedBy from './postedBy';
+import save from './save';
+import comment from './comment';
+
+
+export const schemaTypes = [
+    user,pin,postedBy,comment,save
+]
---
+++ schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy'
+        },
+        {
+            name: 'saveId',
+            title: 'SaveId',
+            type: 'string'
+        }
+    ]
+}
